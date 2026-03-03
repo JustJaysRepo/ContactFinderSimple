3abc4e2da6d4e323d3c8f4607613318031d0a135
JustJay <140190051+JustJaysRepo@users.noreply.github.com>
2026-03-03 12:23:29 -0500

    Initial solution and project scaffolding
    Added .gitignore and created ContactFinder.sln with two console apps (Simple, Advanced) and corresponding xUnit test projects. All projects target .NET 9.0 and include basic starter files. Test projects reference their respective apps and include xUnit and coverlet dependencies.

---
+++ Week01/ContactFinder/tests/ContactFinder.Advanced.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactFinder.Advanced.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Week01/ContactFinder/tests/ContactFinder.Simple.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactFinder.Simple.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
